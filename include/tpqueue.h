17126165826b8123e4352a0529a8e330aa145fe9
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-05-08 21:18:20 +0300

    Implement TPQueue class with push and pop methods

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,62 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+public:
+    TPQueue() :head(nullptr) {}
+    ~TPQueue() {
+        while (head) {
+            Node* next = head->next;
+            delete head;
+            head = next;
+        }
+    }
+
+    T pop() {
+        if (head) {
+            T temp = head->data;
+            Node* next = head->next;
+            delete head;
+            head = next;
+            return temp;
+        } else
+            return T();
+    }
+
+    void push(T data) {
+        if (head) {
+            Node* temp = head;
+            if (head->data.prior < data.prior) {
+                head = create(data);
+                head->next = temp;
+            } else {
+                while (data.prior <= temp->data.prior && (temp->next != nullptr))
+                    temp = temp->next;
+                Node* newNode = create(data);
+                newNode->next = temp->next;
+                temp->next = newNode;
+            }
+        } else
+            head = create(data);
+    }
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+
+    Node* create(const T data) {
+        Node* item = new Node;
+        item->data = data;
+        item->next = nullptr;
+        return item;
+    }
+
+    Node* head;
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
